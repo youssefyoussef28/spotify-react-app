ea70debdeceef9677335af2cdaaf94fa02795019
Youssef Youssef <youssefyoussef28@gmail.com>
2022-10-18 22:52:43 +0300

    Integrated Login with Spotify

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
 
 function App() {
+  const CLIENT_ID = "d8bdc8257b2a46cbbe92d51c547c47a6";
+  const REDIRECT_URI = "http://localhost:3000";
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+  const RESPONSE_TYPE = "token";
+
   return (
     <>
       <h1>Hello world</h1>
+      <a
+        href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
+      >
+        Login to spotify
+      </a>
     </>
   );
 }
