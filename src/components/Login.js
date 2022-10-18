20851d40b150aa537a9a818cc27a5d9792012fcf
Youssef Youssef <youssefyoussef28@gmail.com>
2022-10-19 02:31:34 +0300

    Navbar and Login

---
+++ src/components/Login.js
@@ -0,0 +1,62 @@
+import { useState, useEffect } from "react";
+import LOGO from "../images/LOGO.png";
+
+const Login = () => {
+  const CLIENT_ID = "d8bdc8257b2a46cbbe92d51c547c47a6";
+  const REDIRECT_URI = "http://localhost:3000";
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+  const RESPONSE_TYPE = "token";
+
+  const [token, setToken] = useState("");
+  const [searchKey, setSearchKey] = useState("");
+  const [artists, setArtists] = useState("");
+
+  useEffect(() => {
+    const hash = window.location.hash;
+    let token = window.localStorage.getItem("token");
+
+    if (!token && hash) {
+      token = hash
+        .substring(1)
+        .split("&")
+        .find((elem) => elem.startsWith("access_token"))
+        .split("=")[1];
+
+      window.location.hash = "";
+      window.localStorage.setItem("token", token);
+    }
+
+    setToken(token);
+  }, []);
+
+  const logout = () => {
+    setToken("");
+    window.localStorage.removeItem("token");
+  };
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        {!token ? (
+          <div className="flex items-center justify-center">
+            <a
+              className="flex items-center  text-gray-600 border-gray-400 px-36 border-2 py-3 text-lg rounded-md"
+              href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
+            >
+              Login
+            </a>
+            <img src={LOGO} class="h-12 ml-[-50px]" alt="Spotify Logo" />
+          </div>
+        ) : (
+          <a
+            className="flex items-center  text-gray-600 border-gray-400 px-36 border-2 py-3 text-lg rounded-md cursor-pointer"
+            onClick={logout}
+          >
+            Logout
+          </a>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Login;
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <>
+      <nav class="bg-gray-300 px-2 sm:px-4 py-5 dark:bg-gray-900  w-full border-b border-gray-200 shadow-sm">
+        <div class="container flex justify-between items-center mx-auto">
+          <span class="font-medium text-3xl">Spotify Artist Search</span>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
