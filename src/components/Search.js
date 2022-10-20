b61d5cefb2d3541bd692ce1768da2381df534f6e
Youssef Youssef <youssefyoussef28@gmail.com>
2022-10-20 10:30:31 +0300

    Fixed some Console Errors

--- src/components/Search.js
+++ src/components/Search.js
@@ -53,7 +53,7 @@ const Search = () => {
           {token ? (
             <form className="" onSubmit={searchArtists}>
               <label
-                for="default-search"
+                html="default-search"
                 className="mb-2 text-sm font-medium text-gray-900 sr-only"
               >
                 Search
@@ -70,9 +70,9 @@ const Search = () => {
                     xmlns="http://www.w3.org/2000/svg"
                   >
                     <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
                       d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
                     ></path>
                   </svg>
